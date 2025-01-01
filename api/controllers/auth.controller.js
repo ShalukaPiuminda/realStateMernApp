bd5c1afbfc9126ae607476abbb08a237dffe48df
ShalukaPiuminda <piumindajayaweera@gmail.com>
2025-01-01 13:27:22 +0530

    complete sign up page function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,14 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
   try {
     const { email, username, password } = req.body;
-    const hashedPassword=bcryptjs.hashSync(password,10);
-    const newUser = new User({ email, username, password:hashedPassword }); // Pass an object here
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ email, username, password: hashedPassword }); // Pass an object here
     await newUser.save();
-    res.status(201).json(newUser);
+    res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-   next(error);
+    next(error);
   }
 };
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,40 +1,98 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        return;
+      }
+      setSuccess(data.message);
+      console.log(data);
+      setError(null);
+      setTimeout(() => {
+        navigate("/sign-in");
+      }, 3000);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
-        ></input>
+          onChange={handleChange}
+          value={formData.username}
+        />
         <input
           type="text"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
-        ></input>
+          onChange={handleChange}
+          value={formData.email}
+        />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
-        ></input>
-        <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-90 disabled:opacity-75">
-          SIGN UP
+          onChange={handleChange}
+          value={formData.password}
+        />
+        {error && <p className="text-red-500 mt-5">{error}</p>}
+        {success && <p className="text-green-500 mt-5">{success}</p>}
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-90 disabled:opacity-75"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
         <button className="bg-red-500 text-white p-3 rounded-lg hover:opacity-90 disabled:opacity-75">
-          GOOGlE
+          Google
         </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link to={'/sign-up'}>
-        <span className="text-blue-700">sign in</span>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">sign in</span>
         </Link>
       </div>
     </div>
